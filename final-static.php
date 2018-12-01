f75ae6fe096259422ea0297affa323aeeb016b67
= <=>
2018-12-01 23:15:42 +0700

    namespace

--- final-static.php
+++ final-static.php
@@ -1,16 +1,21 @@
+<?php
+    namespace ConNguoi;
+?>
 <!--
 
  Static là thành ph?n t?nh
  nó nh? bi?n toàn c?c và ???c cho giá tr? sau cùng dù cho x? lý m?t file b?t k? (trong cung 1 chuong trình)
  Nó ???c l?u giá tr? sau cùng mà nó ???c th?c hi?n sau cùng trong l?p
 
+ !important: Self truy xu?t class khai báo nó
+ static truy xu?t ??i t??ng hi?n t?i
  -->
 
 <?php
 
-    Class ConNguoi {
+    class ConNguoi {
 
-        public static $name = "The first";
+        protected static $name = "The first";
 
         public static function setName($name){
 
